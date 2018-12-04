0ad72d646988dc55c973166b9c95e21cd8d7c887
吉井達也 <t.yoshii@entap.co.jp>
2018-12-04 17:05:42 +0900

    build

--- dist/vue-range-slider.scss
+++ dist/vue-range-slider.scss
@@ -235,6 +235,7 @@ $knob-shadow: 1px 1px rgba(0, 0, 0, 0.2) !default;
 
 .right-controller .range-slider-knob .oval-knob-image{
   width: 100%;
+  height: 15px;
   border-radius: 100%;
   box-shadow: 0px 2px 4px 0px rgba(1, 1, 1, 0.5);
   background-color: #2476BC;
